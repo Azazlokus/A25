5627ff3ead138ec8cdeeb9d8d897ff66a1bbd252
Azazlokus <azku@inbox.ru>
2024-09-15 17:25:08 +0700

    Переписал код calculate.php на CleanCode

--- App/calculate.php
+++ App/calculate.php
@@ -1,50 +1,105 @@
 <?php
 namespace App;
-require_once 'Infrastructure/sdbh.php'; use sdbh\sdbh; 
 
-class Calculate
+require_once 'Infrastructure/sdbh.php';
+use sdbh\sdbh;
+
+class ProductRepository
 {
-    public function calculate1()
+    private $dbh;
+
+    public function __construct()
     {
-        $dbh = new sdbh();
-        $days = isset($_POST['days']) ? $_POST['days'] : 0;
-        $product_id = isset($_POST['product']) ? $_POST['product'] : 0;
-        $selected_services = isset($_POST['services']) ? $_POST['services'] : [];
-        $product = $dbh->make_query("SELECT * FROM a25_products WHERE ID = $product_id");
-        if ($product) {
-            $product = $product[0];
-            $price = $product['PRICE'];
-            $tarif = $product['TARIFF'];
-        } else {
+        $this->dbh = new sdbh();
+    }
+
+    public function getProductById($productId)
+    {
+
+        $product = $this->dbh->make_query("SELECT * FROM a25_products WHERE ID = $productId");
+        return $product ? $product[0] : null;
+    }
+}
+
+class PriceCalculator
+{
+    public function calculateTotalPrice($product, $days, $selectedServices)
+    {
+        $price = $product['PRICE'];
+        $tarif = $product['TARIFF'];
+
+        $productPrice = $this->calculateProductPrice($price, $tarif, $days);
+        $servicesPrice = $this->calculateServicesPrice($selectedServices, $days);
+
+        return $productPrice + $servicesPrice;
+    }
+
+    private function calculateProductPrice($basePrice, $tarifSerialized, $days)
+    {
+        $tarifs = unserialize($tarifSerialized);
+
+        if (!is_array($tarifs)) {
+            return $basePrice * $days;
+        }
+
+        $productPrice = $basePrice;
+        foreach ($tarifs as $dayCount => $tarifPrice) {
+            if ($days >= $dayCount) {
+                $productPrice = $tarifPrice;
+            }
+        }
+
+        return $productPrice * $days;
+    }
+
+    private function calculateServicesPrice($selectedServices, $days)
+    {
+        $servicesPrice = 0;
+        foreach ($selectedServices as $service) {
+            $servicesPrice += (float)$service * $days;
+        }
+        return $servicesPrice;
+    }
+}
+
+class CalculateController
+{
+    private $productRepository;
+    private $priceCalculator;
+
+    public function __construct()
+    {
+        $this->productRepository = new ProductRepository();
+        $this->priceCalculator = new PriceCalculator();
+    }
+
+    public function calculate()
+    {
+        $days = $this->getPostValue('days', 0);
+        $productId = $this->getPostValue('product', 0);
+        $selectedServices = $this->getPostValue('services', []);
+
+        $product = $this->productRepository->getProductById($productId);
+
+        if (!$product) {
             echo "Ошибка, товар не найден!";
             return;
         }
 
-        $tarifs = unserialize($tarif);
-        if (is_array($tarifs)) {
-            $product_price = $price;
-            foreach ($tarifs as $day_count => $tarif_price) {
-                if ($days >= $day_count) {
-                    $product_price = $tarif_price;
-                }
-            }
-            $total_price = $product_price * $days;
-        }else{
-            $total_price = $price * $days;
-        }
+        $totalPrice = $this->priceCalculator->calculateTotalPrice($product, $days, $selectedServices);
+        echo $totalPrice;
+    }
 
-        $services_price = 0;
-        foreach ($selected_services as $service) {
-            $services_price += (float)$service * $days;
-        }
-
-        $total_price += $services_price;
-
-        echo $total_price;
+    private function getPostValue($key, $default)
+    {
+        return $_POST[$key] ?? $default;
     }
 }
-
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+// Обработка запроса
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $instance = new Calculate();
-    $instance->calculate1();
+    $controller = new CalculateController();
+    $controller->calculate();
 }
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
 require_once 'App/Infrastructure/sdbh.php'; use sdbh\sdbh;
 $dbh = new sdbh();
 ?>

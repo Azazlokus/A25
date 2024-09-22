60700e36985c2dd8fc0712d4130d9b6eb5ccc7b9
Azazlokus <azku@inbox.ru>
2024-09-23 01:47:59 +0700

    Удалил лишнюю ссылку

--- index.php
+++ index.php
@@ -108,9 +108,6 @@ $products = $dataAdapter->getProducts();
         </div>
     </div>
 </div>
-
-<!-- Скрипты -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 <script src="https://code.jquery.com/ui/1.14.0/jquery-ui.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
